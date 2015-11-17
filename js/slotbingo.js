403a190526d04cfc3672219b476ae9c9637825c6
Ryosuke Fujimaki <fujimaki@qwintet.com>
2015-11-17 00:10:36 +0000

    big num

--- js/slotbingo.js
+++ js/slotbingo.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
 	var select;
 	var columns;
 
+	var tensDigit = $("#tens-digit .wrapper");
+	var onesDigit = $("#ones-digit .wrapper");
+
 	// 当たりの数字を格納。Arr[0]が十の位、Arr[1]が1の位
 	var hitNumberArr = [];
 	// 既出の数字を格納
@@ -12,7 +15,30 @@ $(document).ready(function() {
 	onload(true);
 
 	$("#btn_start").on("click", function(){
-		go();
+		time = parseInt($("#txt_duration").val()) * 1000; // 数値のみの取得
+		$("#btn_start").attr("disabled", "disabled");
+
+		moveSlots(tensDigit);
+		moveSlots(onesDigit);
+
+		$(this).delay(time + 1000).queue(function() {
+			$(this).dequeue();
+			// 当たりの数字は配列に格納されているので、
+			displayAlreadyNum(hitNumberArr[0] * 10 + hitNumberArr[1]);
+
+			tensDigit.children().remove();
+			tensDigit.css({"margin-top" : "-20px"});
+			tensDigit.append("<div class='slot'>" + hitNumberArr[0] + "</div>");
+	
+			onesDigit.children().remove();
+			onesDigit.css({"margin-top" : "-20px"});
+			onesDigit.append("<div class='slot'>" + hitNumberArr[1] + "</div>");
+	
+			$("#btn_start").removeAttr("disabled");
+	
+			hitNumberArr = []; // 当たりの数字の初期化
+			onload(false);
+		})
 	})
 
 	// 受け取った数字を桁ごとの配列に変換して保存
@@ -21,61 +47,28 @@ $(document).ready(function() {
 		hitNumberArr.push(num % 10); // 一の位
 	}
 
+	// 1~75までの数字をランダムで生成
 	function makeRandomNum(){
-		return Math.round(Math.random()*100 % 75);
+		return Math.round(Math.random()*100 % 74) + 1; // 改良
 	}
 
 	//既出の数字を表示
 	function displayAlreadyNum(num){
 		aleadyNumArr.push(num);
-		$('#js-already-num-block').after("<div class='already_num'>"+num+"</div>");
+		$('#js-already-num-block').after("<div class='already_num'>" + num + "</div>");
 	}
 
 	function onload(init) {
 		// 当たりの決定
 		do {
 			var num = makeRandomNum();
-		} while (($.inArray(num, aleadyNumArr) !== -1) && num > 0 && num <=75);
+			console.log(num);
+		} while (($.inArray(num, aleadyNumArr) !== -1));
 		setHitNumberArr(num);
 
-		columns=0;
-		addSlots($("#slots_a .wrapper"), init);
-		addSlots($("#slots_b .wrapper"), init);
-	}
-
-	function go() {
-		time = parseInt($("#txt_duration").val()) * 1000; // 数値のみの取得
-		select = $('#sel_easing').val();
-		$("#btn_start").attr("disabled", "disabled");
-
-		moveSlots($("#slots_a .wrapper"));
-		moveSlots($("#slots_b .wrapper"));
-
-		$(this).delay(time + 1000).queue(function() {
-			$(this).dequeue();
-			// 当たりの数字は配列に格納されているので、
-			displayAlreadyNum(hitNumberArr[0] * 10 + hitNumberArr[1]);
-			reset();
-		})
-	}
-
-	function reset() {
-		$("#slots_a .wrapper").children().remove();
-		$("#slots_a .wrapper").css({
-			"margin-top" : "0px"
-		});
-		$("#slots_a .wrapper").append("<div class='slot'>" + hitNumberArr[0] + "</div>");
-
-		$("#slots_b .wrapper").children().remove();
-		$("#slots_b .wrapper").css({
-			"margin-top" : "0px"
-		});
-		$("#slots_b .wrapper").append("<div class='slot'>" + hitNumberArr[1] + "</div>");
-
-		$("#btn_start").removeAttr("disabled");
-		$("#btn_reset").attr("disabled", "disabled");
-		hitNumberArr = []; // 当たりの数字の初期化
-		onload(false);
+		columns = 0;
+		addSlots(tensDigit, init);
+		addSlots(onesDigit, init);
 	}
 
 	function addSlots(jqo, init) {
@@ -92,10 +85,11 @@ $(document).ready(function() {
 	}
 
 	function moveSlots(jqo) {
+		select = $('#sel_easing').val();
 		time += Math.round(Math.random() * 1000);
 
 		var marginTop = parseInt(jqo.css("margin-top"), 10)
-		marginTop -= (10 * 120)
+		marginTop -= 4800
 
 		jqo.stop(true, true);
 		jqo.animate({

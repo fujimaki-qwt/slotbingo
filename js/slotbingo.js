fe0a6087b2536154a615085149557869b56d5df2
Ryosuke Fujimaki <fujimaki@qwintet.com>
2015-12-02 10:10:03 +0900

    Revert "ramdom animation"
    This reverts commit bd7f4eefe26ebe34fb8b4a8f3b113f38d3c98f54.

--- js/slotbingo.js
+++ js/slotbingo.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-	var easing = ["jswing", "easeInQuad", "easeOutQuad", "easeInOutQuad", "easeInCubic", "easeOutCubic", "easeInOutCubic", "easeInQuart", "easeOutQuart", "easeInOutQuart", "easeInQuint", "easeOutQuint", "easeInOutQuint", "easeInSine", "easeOutSine", "easeInOutSine", "easeInExpo", "easeOutExpo", "easeInOutExpo", "easeInCirc", "easeOutCirc", "easeInOutCirc", "easeInElastic", "easeOutElastic", "easeInOutElastic", "easeInBack", "easeOutBack", "easeInOutBack", "easeInBounce", "easeOutBounce", "easeInOutBounce"];
 	var opts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-	var time = 5 * 1000; // 10の位が決まるまでの秒数
-	var delay = 5 * 1000; // 10の位が決まってから1の位が決まるまでの遅延
+	var time;
+	var select;
 	var columns;
 
 	var tensDigit = $("#tens-digit .wrapper");
@@ -30,28 +29,23 @@ $(document).ready(function() {
 
 	$("#btn_start").on("click", function(){
 		slotAudio.play();
+		time = parseInt($("#txt_duration").val()) * 1000; // 数値のみの取得
 		$("#btn_start").attr("disabled", "disabled");
 
 		moveSlots(tensDigit);
-		moveSlots(onesDigit, delay);
+		moveSlots(onesDigit);
 
-		$(this).delay(time + delay + 200).queue(function() {
-			slotAudio.pause();
-			numberAudio.play();
-
+		$(this).delay(time + 1000).queue(function() {
 			$(this).dequeue();
-
-			// 当たりの数字は配列に格納されているので、復元
+			// 当たりの数字は配列に格納されているので、
 			displayAlreadyNum(hitNumberArr[0] * 10 + hitNumberArr[1]);
 
 			tensDigit.children().remove();
-			tensDigit.css({"margin-top" : "-1200px"});
-			addDummyNum(tensDigit);
+			tensDigit.css({"margin-top" : "0px"});
 			tensDigit.append("<div class='slot'>" + hitNumberArr[0] + "</div>");
 	
 			onesDigit.children().remove();
-			onesDigit.css({"margin-top" : "-1200px"});
-			addDummyNum(onesDigit);
+			onesDigit.css({"margin-top" : "0px"});
 			onesDigit.append("<div class='slot'>" + hitNumberArr[1] + "</div>");
 	
 			$("#btn_start").removeAttr("disabled");
@@ -69,6 +63,10 @@ $(document).ready(function() {
 
 	//既出の数字を表示
 	function displayAlreadyNum(num){
+		
+		slotAudio.pause();
+		numberAudio.play();
+		
 		if(num <= 15){
 			numberColor = 'number_red';
 		}else if(num <= 30){
@@ -80,7 +78,6 @@ $(document).ready(function() {
 		}else{
 			numberColor = 'number_violet';
 		}
-
 		aleadyNumArr.push(num);
 		$('#js-already-num-block').after("<div class='already_num " +numberColor+ "'>"+num+"</div>");
 	}
@@ -89,7 +86,6 @@ $(document).ready(function() {
 		// // 当たりの決定
 		if (numArray.length == 0){
 			alert("終了です orz");
-			$("#btn_start").attr("disabled", "disabled");
 			return;
 		}
 		setHitNumberArr(numArray.pop());
@@ -100,41 +96,32 @@ $(document).ready(function() {
 	}
 
 	function addSlots(jqo, init) {
-		var i = (init == true) ? 0 : 5;
-		for (i; i < 20; i++) {
+		var i = (init == true) ? 0 : 1;
+		for (i; i < 15; i++) {
 			if (i == 12) {
-				jqo.append("<div class='slot hit'>" + hitNumberArr[columns] + "</div>");
+				jqo.append("<div class='slot'>" + hitNumberArr[columns] + "</div>");
 				columns++;
 			} else {
 				var ctr = Math.floor(Math.random() * opts.length);
-				jqo.append("<div class='slot " + i +"'>" + opts[ctr] + "</div>");
+				jqo.append("<div class='slot'>" + opts[ctr] + "</div>");
 			}
 		}
 	}
 
-	function moveSlots(jqo, delay) {
-		if (delay == undefined) {
-			delay = 0;
-		}
+	function moveSlots(jqo) {
+		select = $('#sel_easing').val();
+		time += Math.round(Math.random() * 1000);
 
 		var marginTop = parseInt(jqo.css("margin-top"), 10)
-		marginTop -= jqo.parent().height() * 8
+		marginTop -= jqo.parent().height() * 12
 
 		jqo.stop(true, true);
 		jqo.animate({
 			"margin-top" : marginTop + "px"
 		}, {
-			'duration' : time + delay,
-			'easing' : easing[Math.floor(Math.random() * easing.length)]
+			'duration' : time,
+			'easing' : select
 		});
-	}
-
-	// easeInBack等の特殊なアニメーション対応のための苦肉の策
-	function addDummyNum(jqo) {
-		jqo.append("<div class='slot'>" + opts[Math.floor(Math.random() * opts.length)] + "</div>");
-		jqo.append("<div class='slot'>" + opts[Math.floor(Math.random() * opts.length)] + "</div>");
-		jqo.append("<div class='slot'>" + opts[Math.floor(Math.random() * opts.length)] + "</div>");
-		jqo.append("<div class='slot'>" + opts[Math.floor(Math.random() * opts.length)] + "</div>");
 	}
 
 	function shuffle(array) {
